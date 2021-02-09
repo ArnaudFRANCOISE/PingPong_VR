5e30328352b86505017da4df812ff375bf922cd4
ArnaudFRANCOISE <76952188+ArnaudFRANCOISE@users.noreply.github.com>
2021-02-09 16:26:28 +0100

    Seance du 09/02
    Target trigger fixed
    Score display fixed
    Raquette problem remain

---
+++ New Unity Project/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int score=0;
+    //public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void debug_funct()
+    {
+        Debug.Log("hi it's debugfunct");
+        score ++;
+        Debug.Log(score);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //scoreText.text="SCORE: "+score.ToString();
+        //scoreText.text = "Yolo";
+        Debug.Log(score);
+    }
+}
