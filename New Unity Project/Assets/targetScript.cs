0294818dd1688e649350010ab62411030c20b59a
@ <arnaud.francoise@astudent.yncrea.fr>
2021-02-13 13:36:40 +0100

    Version3

---
+++ New Unity Project/Assets/targetScript.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class targetScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoretxt_box;
+    public GameManager gameManager;
+    public int score = 0; 
+    //private GameManager gmScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretxt_box.text = "SCORE: ";
+        //GameObject gameManager= GameObject.Find("GameManager");
+        //GameManager gmScript=gameManager.GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ontrigger enter");
+        
+        if(other.tag=="ball")
+        {
+            Debug.Log("ball trigger");
+            //gameManager.score++;
+            //gmScript.debug_funct();
+            //Debug.Log(gameManager.score);
+            
+            score++;
+            scoretxt_box.text = "SCORE: " + score.ToString();
+            Debug.Log("ontrigger score: "+ score.ToString());
+        }
+        else
+        {
+            Debug.Log("no enter");
+        }
+    }
+    /*
+    private void Update()
+    {
+        scoretxt_box.text = "SCORE: " + score.ToString();;
+    }*/
+}
